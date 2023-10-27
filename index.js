6dc9280c7dfa1d0279b81892e1a4f330f93432cb
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-10-27 17:08:39 -0300

    Ajustes

--- index.js
+++ index.js
@@ -7,12 +7,16 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.set("view-engine, ejs");
+app.set("view engine", "ejs");
 
 mongoose.connect("mongodb://127.0.0.1:27017/agendamento",{useNewUrlParser: true, useUnifiedTopology: true})
 
 app.get("/", (req, res) => {
     res.send("Route Server on!")
+})
+
+app.get("/cadastro", (req, res) => {
+    res.render("create")
 })
 
 app.listen(8080, () => {})
