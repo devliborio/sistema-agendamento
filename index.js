733302cbb8860c881c2b91b398d6a29f73e829d3
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-10-28 13:59:43 -0300

    Nova rota para o envio de dados para o BD

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const appointmentService = require("./Services/AppointmentService")
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -9,7 +10,7 @@ app.use(bodyParser.json());
 
 app.set("view engine", "ejs");
 
-mongoose.connect("mongodb://127.0.0.1:27017/agendamento",{useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect("mongodb://localhost:27017/agendamento",{useNewUrlParser: true, useUnifiedTopology: true})
 
 app.get("/", (req, res) => {
     res.send("Route Server on!")
@@ -18,5 +19,23 @@ app.get("/", (req, res) => {
 app.get("/cadastro", (req, res) => {
     res.render("create")
 })
+
+app.post("/create", async (req, res) => {
+
+    var status = await appointmentService.Create(
+        req.body.name,
+        req.body.email,
+        req.body.description,
+        req.body.cpf,
+        req.body.date,
+        req.body.time
+        )
+
+    if(status){
+        res.redirect("/")
+    } else {
+        res.send("Ocorreu uma falha!");
+    }
+}); 
 
 app.listen(8080, () => {})
