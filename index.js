d635109d39c9d10837bd8e47f809bb52b4c848af
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-10-28 22:27:30 -0300

    Renderizando o FullCalendar na HomePage

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.set("view engine", "ejs");
 mongoose.connect("mongodb://localhost:27017/agendamento",{useNewUrlParser: true, useUnifiedTopology: true})
 
 app.get("/", (req, res) => {
-    res.send("Route Server on!")
+    res.render("index");
 })
 
 app.get("/cadastro", (req, res) => {
