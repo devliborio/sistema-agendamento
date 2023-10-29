7ad1d166ebddfc304491d0ceb1bfb885faa9e597
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-10-29 16:23:00 -0300

    Renomeando variavel

--- index.js
+++ index.js
@@ -39,8 +39,8 @@ app.post("/create", async (req, res) => {
 }); 
 
 app.get("/getcalendar", async (req, res) => {
-    var appointment = await appointmentService.GetAll(false)
-    res.json(appointment);
+    var appointments = await appointmentService.GetAll(false)
+    res.json(appointments);
 });
 
 app.listen(8080, () => {})
